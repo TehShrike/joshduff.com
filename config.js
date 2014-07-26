54e632d349e19b7f14ead1214d3d2b5346c0dae6
Josh Duff <me@JoshDuff.com>
2014-07-26 13:57:22 -0500

    Fixing edit link

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 noddityConfig = {
 	title: "Josh Duff .com",
 	noddityRoot: window.location.pathname + 'content/',
-	editLink: 'https://github.com/TehShrike/joshduff.com-content/edit/master/',
+	editLink: 'https://github.com/TehShrike/joshduff.com/edit/master/content/',
 	logo: 'http://joshduff.com/content/images/logo.svg',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
