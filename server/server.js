a94b9fe2e4537377ecde8125ed31beed2c9751cb
Josh Duff <me@JoshDuff.com>
2017-02-16 08:59:53 -0600

    No more trying to babel-runtime certain dependencies

--- server/server.js
+++ server/server.js
@@ -2,17 +2,7 @@ require(`babel-polyfill`)
 require(`babel-register`)({
 	plugins: [
 		`transform-async-to-generator`,
-	],
-	ignore: function(path) {
-		// false => transpile with babel
-		const nodeModules = /\/node_modules\//.test(path)
-
-		if (nodeModules) {
-			return !/\/(noddity-static-server|noddity-lazy-static-render)\//.test(path)
-		}
-
-		return false
-	}
+	]
 })
 
 require('./index.js').listen(8989)
