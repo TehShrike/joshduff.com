1532c7375e172d61dc5fc3e865dfc64eb7dcd7bb
Josh Duff <me@JoshDuff.com>
2021-06-18 16:00:30 -0500

    Moving from Up to gh-pages

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-npm ci
-npm run generate
-rm -rf node_modules
-npm i --production
-up --format=plain deploy production
--- server/server.js
+++
@@ -1,5 +0,0 @@
-const port = process.env.PORT
-	? parseInt(process.env.PORT, 10)
-	: 8989
-
-require(`./index.js`)().listen(port)
